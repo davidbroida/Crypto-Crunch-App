8702916892e159ca2d7dbe8ccef4718e17f2d3f2
David Broida <davidcbroida@gmail.com>
2022-10-27 09:32:10 +0800

    configndb

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'nevertell')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
