46220772df6c4bf6bf307d3a04b89235ab5b01dd
David Broida <davidcbroida@gmail.com>
2022-10-28 16:25:31 +0800

    fingers crossed again

--- app.py
+++ app.py
@@ -31,8 +31,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
-# connect_db(app)
-db = SQLAlchemy(app)
+connect_db(app)
 
 ##############################################################################
 # User signup/login/logout
