abdbeb4d18b2791a3f65397bb943c761dd608768
David Broida <davidcbroida@gmail.com>
2022-10-28 20:55:39 +0800

    test

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///capstone_1_db'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "nevertell")
 toolbar = DebugToolbarExtension(app)
