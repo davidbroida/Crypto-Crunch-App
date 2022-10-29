859218884183c81cf64e392d3d46965013199173
David Broida <davidcbroida@gmail.com>
2022-10-29 13:19:48 +0800

    test

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-
+uri = os.environ.get("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///capstone_1_db'))
